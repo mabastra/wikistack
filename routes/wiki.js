281fdcf99d607c7c66663583c1fe09a8f1d7af86
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-03-10 17:31:26 -0500

    showing a wiki page

--- routes/wiki.js
+++ routes/wiki.js
@@ -4,25 +4,38 @@ const { Page } = require('../models');
 const views = require('../views');
 
 router.get('/', async (req, res, next) => {
-    res.send('<h1>Hi</h1>');
-    // res.send(views.wikiPage());
+    const pages = await Page.findAll();
+    res.send(views.main(pages));
 });
+
 router.post('/', async (req, res, next) => {
     try {
         const page = await Page.create({
           title: req.body.title,
           content: req.body.content,
-//          slug: req.body.title
         });
 
         // make sure we only redirect *after* our save is complete! 
         // Don't forget to `await` the previous step. `create` returns a Promise.
-        res.redirect('/');
+        res.redirect(`/wiki/${page.slug}`);
       } catch (error) { next(error) }
 });
+
 router.get('/add', async (req, res, next) => {
     res.send(views.addPage());
 });
+
+router.get('/:slug', async (req, res, next) => {
+    try {
+        const page = await Page.findOne({
+            where: {
+                slug: req.params.slug
+            }
+        });
+        res.send(views.wikiPage(page,'Mab'));
+    } catch(error) {next(error)}
+});
+
 
 Page.beforeValidate( page => {
     page.slug = (page.title) 
@@ -31,7 +44,3 @@ Page.beforeValidate( page => {
 });
 
 module.exports = router;
-
-// const slugify = (title) => {
-//     return (title) ? title.replace('/\s+/g','_').replace(/\W/g, '') : (Math.random() + 1).toString(36).substring(3);
-// }
