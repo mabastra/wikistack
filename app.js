11f1525284770989eaebc6d8e11c5ff41f50b67d
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-03-10 14:59:17 -0500

    sequelize essentials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const views = require('./views/');
 const layout = require("./views/layout");
+const { db } = require('./models');
 
 const app = express();
 
@@ -9,12 +10,25 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
+
 app.get('/', (req, res, next) => {
     res.send(layout(''));
     next();
 });
 
 const PORT = 1221;
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+
+async function initialize () {
+  await db.sync();
+  // await db.sync({force: true});
+
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+}
+
+initialize();
